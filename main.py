48281b16ab3559c39fdfbbe314de4b07d458f57a
cynosure <cynosure@disroot.org>
2025-01-04 21:11:16 -0700

    do you think a depressed person could make this?

---
+++ main.py
@@ -0,0 +1,254 @@
+from enum import Enum
+from rich import print
+from rich import inspect
+
+
+class Capabilities(Enum):
+    CORAL_1 = 1
+    CORAL_2 = 2
+    CORAL_3 = 3
+    CORAL_4 = 4
+    CORAL_HP = 5
+    CORAL_FLOOR = 6
+
+    ALGAE_2 = 7
+    ALGAE_3 = 8
+    ALGAE_FLOOR = 9
+    ALGAE_NET = 10
+    ALGAE_PROC = 11
+
+    CLIMB_LOW = 12
+    CLIMB_HIGH = 11
+
+
+# Place estimates here
+timemap = {
+    Capabilities.CORAL_1: 1,
+    Capabilities.CORAL_2: 2,
+    Capabilities.CORAL_3: 3,
+    Capabilities.CORAL_4: 4,
+    Capabilities.CORAL_HP: 5,
+    Capabilities.CORAL_FLOOR: 6,
+    Capabilities.ALGAE_2: 7,
+    Capabilities.ALGAE_3: 8,
+    Capabilities.ALGAE_FLOOR: 9,
+    Capabilities.ALGAE_NET: 10,
+    Capabilities.ALGAE_PROC: 11,
+    Capabilities.CLIMB_LOW: 12,
+    Capabilities.CLIMB_HIGH: 11,
+}
+
+r1_abilties = [Capabilities.CORAL_FLOOR, Capabilities.CORAL_1]
+r2_abilties = []
+r3_abilties = []
+
+
+class Level:
+    def __init__(self, height):
+        self.height = height
+        if height == 1:
+            self.coral_slots = 3 * 8
+            self.algae_slots = 0
+        elif height == 2:
+            self.algae_slots = 3
+            self.coral_slots = 6
+        elif height == 3:
+            self.algae_slots = 3
+            self.coral_slots = 6
+        elif height == 4:
+            self.algae_slots = 0
+            self.coral_slots = 12
+
+    def take_algae(self) -> bool:  # returns if an algae was drawn
+        if self.algae_slots > 0:
+            self.algae_slots -= 1
+            return True
+        else:
+            return False
+
+    def score_coral(self) -> bool:
+        if self.coral_slots > 0:
+            self.coral_slots -= 1
+            return True
+        else:
+            return False
+
+
+class Reef(object):
+    def __init__(self):
+        self.L1 = Level(1)
+        self.L2 = Level(2)
+        self.L3 = Level(3)
+        self.L4 = Level(4)
+        self.levels = [self.L1, self.L2, self.L3, self.L4]
+
+    def take_algae(self, level):
+        if self.levels[level - 1].take_algae():
+            if level == 2:
+                # taking an algae on l2 frees up 2 slots on l3
+                self.levels[level].coral_slots += 2
+                self.levels[level - 1].coral_slots += 2
+            elif level == 3:
+                # l3 frees up level and level below
+                self.levels[level - 2].coral_slots += 2
+                self.levels[level - 1].coral_slots += 2
+            return True
+        else:
+            return False
+
+    def place_coral(self, level):
+        return self.levels[level - 1].score_coral()
+
+
+reef = Reef()
+
+
+class Robot:
+    def __init__(self, abilties):
+        self.abilties = abilties
+        self.cargo_coral = 1
+        self.cargo_algae = 0
+        # Actions are instant, so after an action occurs, the bot is in a "time debt" where they do nothing until the action is finished
+        self.time_debt = 0
+
+    def teleop(self, time_left):
+        if self.time_debt > 0:
+            self.time_debt -= 1
+            return
+
+        # Priority 1: Climb
+        if (
+            Capabilities.CLIMB_LOW in self.abilties
+            and (time_left + self.time_debt) < 20
+            and (time_left + self.time_debt) > timemap[Capabilities.CLIMB_LOW]
+        ):
+            self.time_debt += timemap[Capabilities.CLIMB_LOW]
+            return "Climbed low"
+
+        elif (
+            Capabilities.CLIMB_HIGH in self.abilties
+            and (time_left + self.time_debt) < 20
+            and (time_left + self.time_debt) > timemap[Capabilities.CLIMB_HIGH]
+        ):
+            self.time_debt += timemap[Capabilities.CLIMB_HIGH]
+            return "Climbed high"
+
+        # Priority 2: Score algae
+
+        elif (
+            self.cargo_algae == 1
+            and Capabilities.ALGAE_NET in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.ALGAE_NET]
+        ):
+            self.time_debt += timemap[Capabilities.ALGAE_NET]
+            self.cargo_algae = 0
+            return "Algae net"
+        elif (
+            self.cargo_algae == 1
+            and Capabilities.ALGAE_PROC in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.ALGAE_PROC]
+        ):
+            self.time_debt += timemap[Capabilities.ALGAE_PROC]
+            self.cargo_algae = 0
+            return "Algae proc"
+
+        elif (
+            self.cargo_algae == 0
+            and Capabilities.ALGAE_2 in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.ALGAE_2]
+            and reef.take_algae(2)
+        ):
+            self.time_debt += timemap[Capabilities.ALGAE_2]
+            self.cargo_algae = 1
+            return "Algae 2"
+
+        elif (
+            self.cargo_algae == 0
+            and Capabilities.ALGAE_3 in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.ALGAE_3]
+            and reef.take_algae(3)
+        ):
+            self.time_debt += timemap[Capabilities.ALGAE_3]
+            self.cargo_algae = 1
+            return "Algae 1"
+
+        # Priority 3: Score coral
+        elif (
+            self.cargo_coral == 1
+            and Capabilities.CORAL_4 in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.CORAL_4]
+            and reef.place_coral(4)
+        ):
+            self.time_debt += timemap[Capabilities.CORAL_4]
+            self.cargo_coral = 0
+            return "Coral 4"
+        elif (
+            self.cargo_coral == 1
+            and Capabilities.CORAL_3 in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.CORAL_3]
+            and reef.place_coral(3)
+        ):
+            self.time_debt += timemap[Capabilities.CORAL_3]
+            self.cargo_coral = 0
+            return "Coral 3"
+        elif (
+            self.cargo_coral == 1
+            and Capabilities.CORAL_2 in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.CORAL_2]
+            and reef.place_coral(2)
+        ):
+            self.time_debt += timemap[Capabilities.CORAL_2]
+            self.cargo_coral = 0
+            return "Coral 2"
+            return
+        elif (
+            self.cargo_coral == 1
+            and Capabilities.CORAL_1 in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.CORAL_1]
+            and reef.place_coral(1)
+        ):
+            self.time_debt += timemap[Capabilities.CORAL_1]
+            self.cargo_coral = 0
+            return "Coral 1"
+            return
+
+        # Intake coral
+
+        elif (
+            self.cargo_coral == 0
+            and Capabilities.CORAL_FLOOR in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.CORAL_FLOOR]
+        ):
+            self.time_debt += timemap[Capabilities.CORAL_FLOOR]
+            self.cargo_coral = 1
+            return "Coral floor"
+        elif (
+            self.cargo_coral == 0
+            and Capabilities.CORAL_HP in self.abilties
+            and (time_left + self.time_debt) > timemap[Capabilities.CORAL_HP]
+        ):
+            self.time_debt += timemap[Capabilities.CORAL_HP]
+            self.cargo_coral = 1
+            return "Coral HP"
+        # Do nothing if all else fails
+        else:
+            return "passed"
+
+
+r1 = Robot(r1_abilties)
+r2 = Robot(r2_abilties)
+r3 = Robot(r3_abilties)
+
+time_left = 135
+r1_buf = [(136, "Connecting to FMS...")]
+r2_buf = [(136, "Connecting to FMS...")]
+r3_buf = [(136, "Connecting to FMS...")]
+while time_left > 0:
+    time_left -= 1
+    r1_buf.append((time_left, r1.teleop(time_left)))
+    r2_buf.append((time_left, r2.teleop(time_left)))
+    r3_buf.append((time_left, r3.teleop(time_left)))
+    print("Time remaining: ", time_left)
+print(r1_buf)
+# If there is high coral to be scored, score it
+# if Capabilities.CORAL_4 in self.abilties and (time_left+self.time_debt) > timemap[Capabilities.CORAL_4]:
